1c2a0f3eb186d207d7cd78edd9700a50ee9cd2d5
fotini <f.batzoni@gmail.com>
2023-03-21 13:55:02 +0200

    Organizing the code

--- OCP--Library/Accounts/ManagerAccounts.cs
+++ OCP--Library/Accounts/ManagerAccounts.cs
@@ -1,9 +1,5 @@
 ﻿using OCPLibrary;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace OCP__Library
 {
---
+++ OCP--Library/Applicants/TechnicianModel.cs
@@ -0,0 +1,12 @@
+﻿using OCPLibrary;
+
+
+namespace OCP__Library.Applicants
+{
+    public class TechnicianModel : IApplicantModel
+    {
+        public string FirstName { get ; set ; }
+        public string LastName { get ; set ; }
+        public IAccounts AccountProcessor { get; set; } = new Accounts();
+    }
+}
