635667bdd3689d13e25c8d6b0f22aa6eaa806ed3
fotini <f.batzoni@gmail.com>
2023-03-21 12:25:15 +0200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using OCPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> applicants = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "Tim", LastName = "Corey"},
+                new PersonModel { FirstName = "Tony", LastName = "Balis"},
+                new PersonModel { FirstName = "Melissa", LastName = "Cortney"},
+            };
+
+
+        }
+    }
+}
