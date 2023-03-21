559e8091b85131e0c57d89bd60a8bbd6a6c218d8
fotini <f.batzoni@gmail.com>
2023-03-21 12:30:04 +0200

    Adding Program.cs Logic

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -18,7 +18,20 @@ namespace ConsoleUI
                 new PersonModel { FirstName = "Melissa", LastName = "Cortney"},
             };
 
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accounProcessor = new Accounts();
 
+            foreach (var person in applicants)
+            {
+                employees.Add(accounProcessor.Create(person));
+            }
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{emp.FirstName} : {emp.LastName} : {emp.EmailAddress}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- OCPLibrary/EmployeeModel.cs
+++ OCPLibrary/EmployeeModel.cs
@@ -8,6 +8,9 @@ namespace OCPLibrary
 {
     public class EmployeeModel
     {
-        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
     }
 }
